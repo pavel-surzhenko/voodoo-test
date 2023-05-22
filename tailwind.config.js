7818b579c9b42293615e4ebd7cb7ec4094576917
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-05-22 19:59:32 +0300

    finish header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ['./src/**/*.{html,js,ts}', './index.html'],
     theme: {
         extend: {},
+        fontFamily: {
+            grotest: ['Space Grotesk', 'sans-serif'],
+        },
     },
     plugins: [],
 };
