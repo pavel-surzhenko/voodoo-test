02119e065627ad10bf1acf372d4607857dabfeb3
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-05-24 00:52:41 +0300

    fix

--- src/generateListHTML.ts
+++ src/generateListHTML.ts
@@ -14,9 +14,9 @@ export function generateListHTML(data: IApiResponse) {
                             <div class="text-xs rounded bg-black text-white p-2 absolute top-3 left-3">USED</div>
                             <div class="px-4 py-6 text-white text-md hidden absolute inset-0 bg-black bg-opacity-80 rounded cursor-pointer">
                                 <h3 class="pb-2" >More info</h3>
-                                <div class="pb-2">Size: ${item.options[0].values}</div>
-                                <div class="pb-2">Colors: ${item.options[1]?.values}</div>
-                                <div>Element: ${item.options[2]?.values}</div>
+                                <div class="pb-2">Size: ${item.options[0]?.values}</div>
+                                <div class="pb-2">Colors: ${item.options[1]?.values ? item.options[1].values : ''}</div>
+                                <div>Element: ${item.options[2]?.values ? item.options[2].values : ''}</div>
                                 <span class="text-red-500 text-xl absolute top-2 right-2">&#10006;</span>
                             </div>
                         </div>
